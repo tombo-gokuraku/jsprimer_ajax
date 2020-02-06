3cb7937115efb6a5b2edfc11f8f8b6f4e289fcdf
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-02-06 10:44:36 +0900

    add result viewer

--- index.js
+++ index.js
@@ -10,10 +10,45 @@ function fetchUserInfo(userId) {
       } else {
         return response.json().then(userInfo => {
           console.log(userInfo);
+          const view = escapeHTML`
+            <h4>${userInfo.name} (@${userInfo.login})</h4>
+            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+            <dl>
+                <dt>Location</dt>
+                <dd>${userInfo.location}</dd>
+                <dt>Repositories</dt>
+                <dd>${userInfo.public_repos}</dd>
+            </dl>
+            `;
+          const result = document.getElementById("result");
+          result.innerHTML = view;
         });
       }
     })
     .catch(error => {
       console.error(error);
     });
+}
+
+// テンプレートリテラル内の文字列を置き換える
+// htmlを表示する際に不正な文字列を変換する
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+// テンプレートリテラルにタグ付けするためのタグ関数
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
 }
